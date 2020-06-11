5cca802ecdfa247f948650ebcf6646f6f4dabe1d
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-06-11 15:40:34 +0100

    added models

---
+++ src/models/ExamsModel.js
@@ -0,0 +1,70 @@
+import { model, Schema } from 'mongoose';
+
+const ExamsModel = new Schema(
+  {
+    course: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: Boolean,
+      default: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    time_allowed: {
+      type: Number,
+      required: true
+    },
+    instructions: {
+      type: String,
+      required: true
+    },
+    questions_per_student: {
+      required: true,
+      type: Number
+    },
+    questions: [
+      {
+        type: {
+          type: Boolean,
+          default: true
+        },
+        correct: {
+          type: String,
+          required: true,
+          default: 'a'
+        },
+        question: {
+          type: String,
+          required: true
+        },
+        options: {
+          a: {
+            type: String,
+            required: true
+          },
+          b: {
+            type: String,
+            required: true
+          },
+          c: {
+            type: String,
+            required: true
+          },
+          d: {
+            type: String,
+            required: true
+          }
+        }
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default model('exams', ExamsModel);
